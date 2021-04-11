fb42265d64aa9f46edbcc645787bc28fee1049c7
Thiago do Prado <thiago@bgcbrasil.com.br>
2021-04-11 15:13:20 -0300

    Init recursive er to afne

--- src/classes/ER.py
+++ src/classes/ER.py
@@ -1,16 +1,19 @@
 from .AFNe import AFNe
 
+def _erToAFNeRecursive(prefixedER):
+  if (prefixedER == ''):
+    return AFNe([], ['q0'], {}, 'q0', [])
+  if (prefixedER == 'E'):
+    return AFNe([], ['q0'], {}, 'q0', ['q0'])
+  if (len(prefixedER) == 1):
+    return AFNe([prefixedER], ['q0', 'q1'], { 'q0': [[prefixedER, 'q1']]}, 'q0', ['q1'])
+  
+  return AFNe()
+
 class ER:
   def __init__(self, prefixedER):
     self.prefixedER = prefixedER
 
   @staticmethod
   def erToAFNe(er):
-    if (er.prefixedER == ''):
-      return AFNe([], ['q0'], {}, 'q0', [])
-    if (er.prefixedER == 'E'):
-      return AFNe([], ['q0'], {}, 'q0', ['q0'])
-    if (len(er.prefixedER) == 1):
-      return AFNe([er.prefixedER], ['q0', 'q1'], { 'q0': [[er.prefixedER, 'q1']]}, 'q0', ['q1'])
-    
-    return AFNe()
+    return _erToAFNeRecursive(er.prefixedER)
